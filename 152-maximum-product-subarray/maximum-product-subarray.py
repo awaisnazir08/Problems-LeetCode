1c5f4ed27a57ad71e8b1a25b50b967860fc0dab7
Awais Nazir (Little_One) <113518103+awaisnazir08@users.noreply.github.com>
2024-03-13 23:25:58 +0500

    Time: 36 ms (99.88%) | Memory: 12.1 MB (56.31%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -4,19 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # my_dict = {}
-        # i = 0, j = 1
         n = len(nums)
-        # for i in range(0, n):
-        #     for j in range(i, n):
-        #         if my_dict[ij] in my_dict:
-
-        # product = 0
-        # if len(nums) == 1:
-        #     return nums[0]
-        # j = nums[1]
-        # for i in nums[1:]:
-        #     new_product = i * j
         max_product = float('-inf')
         product = 1
         if n == 1:
@@ -26,8 +14,6 @@ class Solution(object):
                 product = 1
                 continue
             product *= nums[i]
-            # if(product == 0):
-            #     product = 1
             max_product = max(product, max_product)
         j = n - 1
         product = 1
@@ -43,8 +29,3 @@ class Solution(object):
         if(max_element > max_product):
             max_product = max_element
         return max_product
-            
-
-
-
-        
