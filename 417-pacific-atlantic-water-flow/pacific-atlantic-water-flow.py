0423461c54ee0dedd358de9d7cfd6ed327acc826
Awais Nazir (Little_One) <113518103+awaisnazir08@users.noreply.github.com>
2024-03-19 01:32:13 +0500

    Time: 212 ms (57.48%) | Memory: 18.1 MB (55.52%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,35 +1,44 @@
-class Solution:
-    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        ROWS, COLS = len(heights), len(heights[0])
-        pac, atl = set(), set()
 
-        def dfs(r, c, visit, prevHeight):
-            if (
-                (r, c) in visit
-                or r < 0
-                or c < 0
-                or r == ROWS
-                or c == COLS
-                or heights[r][c] < prevHeight
-            ):
-                return
-            visit.add((r, c))
-            dfs(r + 1, c, visit, heights[r][c])
-            dfs(r - 1, c, visit, heights[r][c])
-            dfs(r, c + 1, visit, heights[r][c])
-            dfs(r, c - 1, visit, heights[r][c])
+class Solution(object):
+    def pacificAtlantic(self, heights):
+        """
+        :type heights: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        pacific, atlantic = set(), set()
+        rows, cols = len(heights), len(heights[0])
 
-        for c in range(COLS):
-            dfs(0, c, pac, heights[0][c])
-            dfs(ROWS - 1, c, atl, heights[ROWS - 1][c])
+        def bfs(row, col, visit, element):
+            my_queue = [(row, col, element)]
+            # visit.add((row, col))
+            while my_queue:
+                current = my_queue.pop(0)
+                row, col, value = current
+                if (row < 0 or col < 0 or row == rows or col == cols or (row, col) in visit):
+                    continue
+                else:
+                    visit.add((row, col))
+                    if row + 1 < rows and heights[row + 1][col] >= value:
+                        my_queue.append((row + 1, col, heights[row + 1][col]))
+                    if row - 1 >= 0 and heights[row - 1][col] >= value:
+                        my_queue.append((row - 1, col, heights[row - 1][col]))
+                    if col + 1 < cols and heights[row][col + 1] >= value:
+                        my_queue.append((row, col + 1, heights[row][col + 1]))
+                    if col - 1 >= 0 and heights[row][col - 1] >= value:
+                        my_queue.append((row, col - 1, heights[row][col - 1]))
 
-        for r in range(ROWS):
-            dfs(r, 0, pac, heights[r][0])
-            dfs(r, COLS - 1, atl, heights[r][COLS - 1])
 
-        res = []
-        for r in range(ROWS):
-            for c in range(COLS):
-                if (r, c) in pac and (r, c) in atl:
-                    res.append([r, c])
-        return res
+        for row in range(rows):
+            bfs(row, 0, pacific, heights[row][0])
+            bfs(row, cols - 1, atlantic, heights[row][cols - 1])
+
+        for col in range(cols):
+            bfs(0, col, pacific, heights[0][col])
+            bfs(rows - 1, col, atlantic, heights[rows - 1][col])
+
+        result = []
+        for r in range(rows):
+            for c in range(cols):
+                if (r, c) in pacific and (r, c) in atlantic:
+                    result.append([r, c])
+        return result
