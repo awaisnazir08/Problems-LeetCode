6cfb7e5ecf2cdc0055260f562fe9b2a5b3a9845b
Awais Nazir (Little_One) <113518103+awaisnazir08@users.noreply.github.com>
2024-04-28 13:38:01 +0500

    Time: 341 ms (89.20%) | Memory: 16.9 MB (88.80%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.py
+++ 917-boats-to-save-people/boats-to-save-people.py
@@ -5,36 +5,48 @@ class Solution(object):
         :type limit: int
         :rtype: int
         """
-        people.sort(reverse = True)
-        dic = {}
-        for person in people:
-            if person not in dic:
-                dic[person] = 1
-            else:
-                dic[person] += 1
-        boats = 0
-        for person in people:
-            if dic[person] == 0:
-                continue
-            dic[person] -= 1
-            if person == limit:
-                boats += 1
-                continue
-            elif person < limit:
-                rem = limit - person
-                temp = rem
-                while True:
-                    if temp in dic and dic[temp] > 0:
-                        dic[temp] -= 1
-                        boats += 1
-                        break
-                    else:
-                        temp -= 1
-                        if temp == 0:
-                            boats += 1
-                            break
 
-        return boats
+        """Solution 1"""
+        # people.sort(reverse = True)
+        # dic = {}
+        # for person in people:
+        #     if person not in dic:
+        #         dic[person] = 1
+        #     else:
+        #         dic[person] += 1
+        # boats = 0
+        # for person in people:
+        #     if dic[person] == 0:
+        #         continue
+        #     dic[person] -= 1
+        #     if person == limit:
+        #         boats += 1
+        #         continue
+        #     elif person < limit:
+        #         rem = limit - person
+        #         temp = rem
+        #         while True:
+        #             if temp in dic and dic[temp] > 0:
+        #                 dic[temp] -= 1
+        #                 boats += 1
+        #                 break
+        #             else:
+        #                 temp -= 1
+        #                 if temp == 0:
+        #                     boats += 1
+        #                     break
+
+        # return boats
+        """ Solution 2"""
+        people.sort()
+        right = len(people) - 1
+        left = res = 0
+        while left <= right:
+            res += 1
+            if people[left] + people[right] <= limit:
+                left += 1
+            right -= 1
+        return res
 
 
 
