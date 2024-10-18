9b2794d6b4aa69fe808064547f486beb30e1fb9c
Awais Nazir (Little_One) <113518103+awaisnazir08@users.noreply.github.com>
2024-10-18 12:41:15 +0500

    Time: 0 ms (100.00%) | Memory: 11.7 MB (34.40%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -4,10 +4,14 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
+
+        # take transpose
         for i in range(len(matrix)):
             for j in range(i, len(matrix)):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
         
+
+        # then reverse the rows (actually reversing the columns)
         for i in range(len(matrix)):
             matrix[i].reverse()
                 
