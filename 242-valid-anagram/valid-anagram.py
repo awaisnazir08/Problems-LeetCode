bf7157f7148f97b457b06ed314f85afcc67445cd
Awais Nazir (Little_One) <113518103+awaisnazir08@users.noreply.github.com>
2025-09-11 14:59:26 +0500

    Time: 23 ms (56.17%) | Memory: 13.4 MB (49.97%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        s = sorted(s)
+        t = sorted(t)
+
+        return s == t
+
+        
