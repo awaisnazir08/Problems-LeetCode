2737cc1e6d713e67b1bccda8eef30dccbad3766f
Awais Nazir (Little_One) <113518103+awaisnazir08@users.noreply.github.com>
2025-10-07 19:42:30 +0500

    Time: 498 ms (6.14%) | Memory: 13.5 MB (71.37%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,4 +1,5 @@
 import math
+
 class Solution(object):
     def minEatingSpeed(self, piles, h):
         """
@@ -6,29 +7,22 @@ class Solution(object):
         :type h: int
         :rtype: int
         """
-        highest = max(piles)
-
-        left = 1
-        right = highest
-
+        left, right = 1, max(piles)
         res = right
 
         while left <= right:
             middle = (left + right) // 2
-
             current_hours = 0
 
             for pile in piles:
-                current_hours += int(math.ceil(pile / float(middle)))
-
-                if current_hours > h:
-                    left = middle + 1
+                current_hours += math.ceil(pile / float(middle))
+                if current_hours > h:  # optimization: stop early
                     break
 
-
             if current_hours <= h:
-                res = min(res, middle)
-
+                res = middle
                 right = middle - 1
+            else:
+                left = middle + 1
 
-        return int(res)
+        return res
