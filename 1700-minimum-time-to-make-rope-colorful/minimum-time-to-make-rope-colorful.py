1eade82708c3eafa8b99bb3ab3802dfadfe5f7dd
Awais Nazir (Little_One) <113518103+awaisnazir08@users.noreply.github.com>
2025-11-03 12:27:16 +0000

    Time: 163 ms (29.27%) | Memory: 19 MB (82.11%) - LeetSync

---
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def minCost(self, colors, neededTime):
+        """
+        :type colors: str
+        :type neededTime: List[int]
+        :rtype: int
+        """
+
+        n = len(colors)
+
+        l = 0
+
+        r = 0
+        res = 0
+
+        current = 0
+        while r < n:
+            if colors[r] == colors[l]:
+                current = max(neededTime[l], neededTime[r], current)
+                res += neededTime[r]
+            else:
+                res -= current
+                l = r
+                current = 0
+                current = max(neededTime[l], neededTime[r], current)
+                res += neededTime[r]
+            r += 1
+        
+        return res - current
+
+
+                
+        
